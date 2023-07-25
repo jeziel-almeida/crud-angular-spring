2445f9212616ab18dbba9f4d8e56efbdc6dae294
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-07-25 12:37:01 -0300

    JsonProperty in Course

--- crud-spring/src/main/java/com/projetocrud/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/projetocrud/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.projetocrud.crudspring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +15,7 @@ public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("_id")
     private Long id;
 
     //Varchar(200) e NOT NULL

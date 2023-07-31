7bc78b79e20fb902dfa3c37e606f2066b8870858
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-07-31 18:05:43 -0300

    Post method

--- crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
@@ -2,10 +2,12 @@ package com.projetocrud.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetocrud.crudspring.model.Course;
@@ -26,7 +28,8 @@ public class CourseController {
     }
 
     @PostMapping
-    public void create(@RequestBody Course course) {
-        courseRepository.save(course);
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }

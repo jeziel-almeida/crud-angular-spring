3ce56378908cf5dbe2a30c2679ec8b2c68d1dd53
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-08-08 14:39:53 -0300

    Get Course By Name and By Id

--- crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.projetocrud.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,9 +29,27 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{name}")
+    public ResponseEntity<Course> getByName(@PathVariable String name) {
+        Course course = courseRepository.findItemByName(name);
+        if(course != null) {
+            return ResponseEntity.ok().body(course);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getById(@PathVariable String id) {
+        return courseRepository.findById(id)
+            .map(course -> ResponseEntity.ok().body(course))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
     }
+
 }

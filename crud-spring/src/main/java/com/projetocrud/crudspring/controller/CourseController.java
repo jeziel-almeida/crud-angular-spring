9d4662e9a178c6a681b6134404997d7760e9b47a
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-08-12 14:36:35 -0300

    Update Endpoint

--- crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/projetocrud/crudspring/controller/CourseController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,7 +44,7 @@ public class CourseController {
     @GetMapping("/{id}")
     public ResponseEntity<Course> getById(@PathVariable String id) {
         return courseRepository.findById(id)
-            .map(course -> ResponseEntity.ok().body(course))
+            .map(recordFound -> ResponseEntity.ok().body(recordFound))
             .orElse(ResponseEntity.notFound().build());
     }
 
@@ -50,6 +52,23 @@ public class CourseController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable String id, @RequestBody Course course) {
+        return courseRepository.findById(id)
+            .map(recordFound -> {
+                recordFound.setName(course.getName());
+                recordFound.setCategory(course.getCategory());
+                Course updated = courseRepository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        courseRepository.deleteById(id);
     }
 
 }

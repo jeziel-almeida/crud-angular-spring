00dce19b2b4eb974b5f0421aa6e41c28ac78790d
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-08-20 23:14:00 -0300

    Length and nullable validators

--- crud-spring/src/main/java/com/projetocrud/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/projetocrud/crudspring/model/Course.java
@@ -2,17 +2,22 @@ package com.projetocrud.crudspring.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Document("courses")
 public class Course {
-    
+
     @Id
     private String id;
+    //@GeneratedValue(strategy = GenerationType.AUTO)
+    //JsonProperty("_id")
 
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @Column(length = 50, nullable = false)
     private String category;
 }
